a582d3da9e14bed6f8395260ef150e3aa006c4d4
novelkhan <novel4004@gmail.com>
2025-06-10 13:44:50 +0600

    Working properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'EmployeeManagementFrontend';
+  constructor(private http: HttpClient) {}
+
+  downloadReport() {
+    this.http.get('https://localhost:7152/api/Report/employee-report', { responseType: 'blob' })
+      .subscribe((blob: Blob) => {
+        saveAs(blob, 'EmployeeReport.pdf');
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
